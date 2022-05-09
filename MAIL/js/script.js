75b74a225942136dde67aed5faf8aec6b9a26ccd
Tommaso <tommaso.lodesani@gmail.com>
2022-05-09 17:01:18 +0200

    email

---
+++ MAIL/js/script.js
@@ -0,0 +1,30 @@
+const myInput = document.querySelector("input");
+
+const myButton = document.querySelector("button");
+
+const myOutput = document.querySelector(".risultato");
+
+const listaEmail = ["tommaso@gmail.com", "luca@gmail.com", "anna@gmail.com", "giulia@gmail.com"];
+
+myButton.addEventListener("click",
+
+    function () {
+
+        let emailUtente = myInput.value;
+        
+
+        for(i = 0; i < listaEmail.length; i++){
+            console.log(listaEmail[i]);
+
+            if(emailUtente == listaEmail[i]){
+                myOutput.innerHTML = `Benvenuto`;
+                
+            }
+        }
+
+
+
+
+    }
+
+)
